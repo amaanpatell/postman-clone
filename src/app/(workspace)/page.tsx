546d912d43dba0461ab472146f27cc7135c23907
Amaan Patel <amaanp32@gmail.com>
2025-10-14 15:43:51 +0530

    added collection ui

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -5,25 +5,25 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-// import { useWorkspaceStore } from "@/modules/Layout/store";
+import { useWorkspaceStore } from "@/modules/Layout/store";
+import TabbedSidebar from "@/modules/workspace/components/sidebar";
 // import RequestPlayground from "@/modules/request/components/request-playground";
 
-// import TabbedSidebar from "@/modules/workspace/components/sidebar";
 
-// import { useGetWorkspace } from "@/modules/workspace/hooks/workspace";
+import { useGetWorkspace } from "@/modules/workspace/hooks/workspace";
 import { Loader } from "lucide-react";
 
 const Page = () => {
-  // const { selectedWorkspace } = useWorkspaceStore();
-  // const { data: currentWorkspace, isLoading } = useGetWorkspace( selectedWorkspace?.id!);
+  const { selectedWorkspace } = useWorkspaceStore();
+  const { data: currentWorkspace, isLoading } = useGetWorkspace( selectedWorkspace?.id!);
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex flex-col items-center justify-center h-full">
-  //       <Loader className="animate-spin h-6 w-6 text-indigo-500" />
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Loader className="animate-spin h-6 w-6 text-indigo-500" />
+      </div>
+    );
+  }
 
 return (
   <ResizablePanelGroup direction="horizontal">
@@ -35,7 +35,7 @@ return (
 
     <ResizablePanel defaultSize={35} maxSize={40} minSize={25} className="flex">
       <div className="flex-1">
-        {/* <TabbedSidebar currentWorkspace={currentWorkspace} /> */}
+        <TabbedSidebar currentWorkspace={currentWorkspace} />
       </div>
     </ResizablePanel>
   </ResizablePanelGroup>
---
+++ src/modules/collections/actions/index.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import db from "@/lib/db";
+
+export const createCollection = async (workspaceId: string, name: string) => {
+  const collection = await db.collection.create({
+    data: {
+      name,
+      workspace: {
+        connect: {
+          id: workspaceId,
+        },
+      },
+    },
+  });
+  return collection;
+};
+
+export const getCollection = async (workspaceId: string) => {
+  const collection = await db.collection.findMany({
+    where: {
+      workspaceId,
+    },
+  });
+  return collection;
+};
+
+export const deleteCollection = async (collectionId: string) => {
+  await db.collection.delete({
+    where: {
+      id: collectionId,
+    },
+  });
+};
+
+export const editCollection = async (collectionId: string, name: string) => {
+  const collection = await db.collection.update({
+    where: {
+      id: collectionId,
+    },
+    data: {
+      name,
+    },
+  });
+};
